09dab83c3fb24d863ae6eb88ced7a1a2bfc194c1
Sandor Martin Leyva <sandormartin37@gmail.com>
2019-02-10 22:51:12 -0500

    page obras y servicios updated

--- README.md
+++ README.md
@@ -1,17 +1,22 @@
 # WordPress
 Landing page in wordpress
 
-db-name: dreyna
+## db-name: 
+    dreyna
+### user:
+	sandor
+### pass
+	s12345678
 
-##plugins:
-contact-form-7
-elementor
-one-click-child-theme
+## plugins:
+    contact-form-7
+    elementor
+    one-click-child-theme
 
-##theme:
-audioman
-###child-theme
-dreyna
+## theme:
+    audioman
+### child-theme
+    dreyna
 
 
 php version: 5.4+
--- wp-content/themes/dreyna/functions.php
+++ wp-content/themes/dreyna/functions.php
@@ -6,11 +6,31 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array('parent-style')
-    );
+    wp_enqueue_style( 'bootstrap',get_stylesheet_directory_uri() . '/bootstrap.min.css');
+    wp_enqueue_style( 'child-style',get_stylesheet_directory_uri() . '/style.css',array('parent-style'));
 }
 //
 // Your code goes below
 //
+function wpb_hook_javascript(){
+	?>
+	<script type="text/javascript">
+		document.addEventListener('DOMContentLoaded', function(event){
+
+			var b = document.getElementsByClassName('attachment-medium');
+			b[0].classList.add("img-responsive");
+			b[1].classList.add("img-responsive");
+			b[2].classList.add("img-responsive");
+
+			var a = document.getElementsByClassName("comment");
+			a[0].children[0].classList.remove('listing-item');
+			a[0].children[0].classList.add("col-md-6");
+			a[0].children[1].classList.remove('listing-item');
+			a[0].children[1].classList.add("col-md-6");
+		});
+		
+	</script>
+	<?php
+}
+
+add_action('wp_head','wpb_hook_javascript');
